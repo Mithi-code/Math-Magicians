499f0e3d6f78ecf0ef0ac3a1ed6496e24cebf2b4
mithlesh kumar <mithlesh.9m@gmail.com>
2022-01-04 23:04:00 +0530

    fix linter error

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
-import React, { Component } from 'react';
-import './App.css';
-import Calculator from './components/Calculator';
+import React, { Component } from "react";
+import "./App.css";
+import Calculator from "./components/Calculator";
 
 // eslint-disable-next-line react/prefer-stateless-function
 class App extends Component {
   render() {
-    return (<div>
-      <h1>Calculator</h1>
-      <Calculator />
-    </div>);
+    return (
+      <div>
+        <h1>Calculator</h1>
+        <Calculator />
+      </div>
+    );
   }
 }
 export default App;
--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,44 +1,45 @@
-import React, { Component } from 'react';
-import './Calculator.css';
+import React, { Component } from "react";
+import "./Calculator.css";
 
 // eslint-disable-next-line react/prefer-stateless-function
 class Calculator extends Component {
   render() {
     return (
       <div className="calculator">
-          <div className="result">0</div>
-          <table>
-                  <tr>
-                   <td>AC</td>
-                   <td>+/-</td>
-                   <td>%</td>
-                   <td>+</td>
-                  </tr>
-                  <tr>
-                    <td>7</td>
-                    <td>8</td>
-                    <td>9</td>
-                    <td>×</td>
-                  </tr>
-                  <tr>
-                    <td>4</td>
-                    <td>5</td>
-                    <td>6</td>
-                    <td>-</td>
-                  </tr>
-                  <tr>
-                    <td>1</td>
-                    <td>2</td>
-                    <td>3</td>
-                    <td>+</td>
-                  </tr>
-                  <tr>
-                    <td colSpan={2}>0</td>
-                    <td>.</td>
-                    <td>=</td>
-                  </tr>
-          </table>
-        </div>);
+        <div className="result">0</div>
+        <table>
+          <tr>
+            <td>AC</td>
+            <td>+/-</td>
+            <td>%</td>
+            <td>+</td>
+          </tr>
+          <tr>
+            <td>7</td>
+            <td>8</td>
+            <td>9</td>
+            <td>×</td>
+          </tr>
+          <tr>
+            <td>4</td>
+            <td>5</td>
+            <td>6</td>
+            <td>-</td>
+          </tr>
+          <tr>
+            <td>1</td>
+            <td>2</td>
+            <td>3</td>
+            <td>+</td>
+          </tr>
+          <tr>
+            <td colSpan={2}>0</td>
+            <td>.</td>
+            <td>=</td>
+          </tr>
+        </table>
+      </div>
+    );
   }
 }
 
