12813f1bebb7d2d9a719071c0e2630342544c061
mithlesh kumar <mithlesh.9m@gmail.com>
2022-01-05 23:31:53 +0530

    linter error

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -13,122 +13,122 @@ class Calculator extends Component {
     };
     this.handleClick = this.handleClick.bind(this);
   }
+
   handleClick(e) {
     if (!e.target.name) return;
     const data = calculate(this.state, e.target.name);
-    console.log(data);
     this.setState({ ...data });
   }
   render() {
     return (
-      <div className='calculator'>
+      <div className="calculator">
         {this.state.next ? (
-          <div className='result'>{this.state.next}</div>
+          <div className="result">{this.state.next}</div>
         ) : (
-          <div className='result'>{this.state.total}</div>
+          <div className="result">{this.state.total}</div>
         )}
         <table onClick={this.handleClick}>
           <tr>
             <td>
-              <button type='button' name='AC'>
+              <button type="button" name="AC">
                 AC
               </button>
             </td>
             <td>
-              <button type='button' name='+/-'>
+              <button type="button" name="+/-">
                 +/-
               </button>
             </td>
             <td>
-              <button type='button' name='%'>
+              <button type="button" name="%">
                 %
               </button>
             </td>
             <td>
-              <button type='button' name='+'>
+              <button type="button" name="+">
                 +
               </button>
             </td>
           </tr>
           <tr>
             <td>
-              <button type='button' name='7'>
+              <button type="button" name="7">
                 7
               </button>
             </td>
             <td>
-              <button type='button' name='8'>
+              <button type="button" name="8">
                 8
               </button>
             </td>
             <td>
-              <button type='button' name='9'>
+              <button type="button" name="9">
                 9
               </button>
             </td>
             <td>
-              <button type='button' className='operator' name='x'>
+              <button type="button" className="operator" name="x">
                 x
               </button>
             </td>
           </tr>
           <tr>
             <td>
-              <button type='button' name='4'>
+              <button type="button" name="4">
                 4
               </button>
             </td>
             <td>
-              <button type='button' name='5'>
+              <button type="button" name="5">
                 5
               </button>
             </td>
             <td>
-              <button type='button' name='6'>
+              <button type="button" name="6">
                 6
               </button>
             </td>
             <td>
-              <button type='button' name='-'>
+              <button type="button" name="-">
                 -
               </button>
             </td>
           </tr>
           <tr>
             <td>
-              <button type='button' name='1'>
+              <button type="button" name="1">
                 1
               </button>
             </td>
             <td>
-              <button type='button' name='2'>
+              <button type="button" name="2">
                 2
               </button>
             </td>
             <td>
-              <button type='button' name='3'>
+              <button type="button" name="3">
                 3
               </button>
             </td>
             <td>
-              <button type='button' name='+'>
+              <button type="button" name="+">
                 +
               </button>
             </td>
           </tr>
           <tr>
             <td colSpan={2}>
-              <button type='button' name='0'>
+              <button type="button" name="0">
                 0
               </button>
             </td>
             <td>
-              <button type='button' name='.'>
+              <button type="button" name=".">
                 .
               </button>
             </td>
             <td>
-              <button type='button' name='='>
+              <button type="button" name="=">
                 =
               </button>
             </td>
