e58f4cd182907e7a13f65798e31c1065dab3b0e6
mithlesh kumar <mithlesh.9m@gmail.com>
2022-01-06 15:42:14 +0530

    changes according to reviewer

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import './Calculator.css';
 import calculate from '../logic/calculate';
 
-// eslint-disable-next-line react/prefer-stateless-function
 class Calculator extends Component {
   constructor(props) {
     super(props);
@@ -21,116 +20,116 @@ class Calculator extends Component {
   }
 
   render() {
-    /* eslint-disable */
+    const {next , total} = this.state
     return (
       <div className="calculator">
-        {this.state.next ? (
-          <div className="result">{this.state.next}</div>
+        {next ? (
+          <div className="result">{next}</div>
         ) : (
-          <div className="result">{this.state.total}</div>
+          <div className="result">{total}</div>
         )}
-        <table onClick={this.handleClick}>
+        <table>
           <tr>
             <td>
-              <button type="button" name="AC">
+              <button type="button" name="AC" onClick={this.handleClick}>
                 AC
               </button>
             </td>
             <td>
-              <button type="button" name="+/-">
+              <button type="button" name="+/-" onClick={this.handleClick}>
                 +/-
               </button>
             </td>
             <td>
-              <button type="button" name="%">
+              <button type="button" name="%" onClick={this.handleClick}>
                 %
               </button>
             </td>
             <td>
-              <button type="button" name="+">
+              <button type="button" name="+" onClick={this.handleClick}>
                 +
               </button>
             </td>
           </tr>
           <tr>
             <td>
-              <button type="button" name="7">
+              <button type="button" name="7" onClick={this.handleClick}>
                 7
               </button>
             </td>
             <td>
-              <button type="button" name="8">
+              <button type="button" name="8" onClick={this.handleClick}>
                 8
               </button>
             </td>
             <td>
-              <button type="button" name="9">
+              <button type="button" name="9" onClick={this.handleClick}>
                 9
               </button>
             </td>
             <td>
-              <button type="button" className="operator" name="x">
+              <button type="button" className="operator" name="x" onClick={this.handleClick}>
                 x
               </button>
             </td>
           </tr>
           <tr>
             <td>
-              <button type="button" name="4">
+              <button type="button" name="4" onClick={this.handleClick}>
                 4
               </button>
             </td>
             <td>
-              <button type="button" name="5">
+              <button type="button" name="5" onClick={this.handleClick}>
                 5
               </button>
             </td>
             <td>
-              <button type="button" name="6">
+              <button type="button" name="6" onClick={this.handleClick}>
                 6
               </button>
             </td>
             <td>
-              <button type="button" name="-">
+              <button type="button" name="-" onClick={this.handleClick}>
                 -
               </button>
             </td>
           </tr>
           <tr>
             <td>
-              <button type="button" name="1">
+              <button type="button" name="1" onClick={this.handleClick}>
                 1
               </button>
             </td>
             <td>
-              <button type="button" name="2">
+              <button type="button" name="2" onClick={this.handleClick}>
                 2
               </button>
             </td>
             <td>
-              <button type="button" name="3">
+              <button type="button" name="3" onClick={this.handleClick}>
                 3
               </button>
             </td>
             <td>
-              <button type="button" name="+">
+              <button type="button" name="+" onClick={this.handleClick}>
                 +
               </button>
             </td>
           </tr>
           <tr>
             <td colSpan={2}>
-              <button type="button" name="0">
+              <button type="button" name="0" onClick={this.handleClick}>
                 0
               </button>
             </td>
             <td>
-              <button type="button" name=".">
+              <button type="button" name="." onClick={this.handleClick}>
                 .
               </button>
             </td>
             <td>
-              <button type="button" name="=">
+              <button type="button" name="=" onClick={this.handleClick}>
                 =
               </button>
             </td>
